c660c913ed936978b41f7af3c90b69b43012767b
Yilun Xiao <yilunxiao2@gmail.com>
2021-10-03 13:18:33 -0400

    added methhods to implementation modules for projectcolumn and resourcedetail

--- src/main/java/com/itlizeproject/ItlizeProjectManager/Service/Impl/ProjectColumnServiceImp.java
+++ src/main/java/com/itlizeproject/ItlizeProjectManager/Service/Impl/ProjectColumnServiceImp.java
@@ -1,4 +1,36 @@
 package com.itlizeproject.ItlizeProjectManager.Service.Impl;
 
-public class ProjectColumnServiceImp {
+import com.itlizeproject.ItlizeProjectManager.Entity.ProjectColumn;
+import com.itlizeproject.ItlizeProjectManager.Repository.ProjectColumnRepository;
+import com.itlizeproject.ItlizeProjectManager.Service.ProjectColumnService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProjectColumnServiceImp implements ProjectColumnService {
+    @Autowired
+    private ProjectColumnRepository repository;
+
+    @Override
+    public ProjectColumn findById(Integer Id) {
+        return repository.getById(Id);
+    }
+
+    @Override
+    public List<ProjectColumn> findByProjectId(Integer projectId) {
+        return null;
+        // return repository.findByProjectId(projectId);
+    }
+
+    @Override
+    public List<ProjectColumn> findAll() {
+        return repository.findAll();
+    }
+
+    @Override
+    public ProjectColumn save(ProjectColumn projectColumn) {
+        return repository.save(projectColumn);
+    }
 }
--- src/main/java/com/itlizeproject/ItlizeProjectManager/Service/Impl/ResourceDetailServiceImp.java
+++ src/main/java/com/itlizeproject/ItlizeProjectManager/Service/Impl/ResourceDetailServiceImp.java
@@ -1,4 +1,43 @@
 package com.itlizeproject.ItlizeProjectManager.Service.Impl;
 
-public class ResourceDetailServiceImp {
+import com.itlizeproject.ItlizeProjectManager.Entity.ResourceDetail;
+import com.itlizeproject.ItlizeProjectManager.Repository.ResourceDetailRepository;
+import com.itlizeproject.ItlizeProjectManager.Service.ResourceDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ResourceDetailServiceImp implements ResourceDetailService {
+    @Autowired
+    private ResourceDetailRepository repository;
+
+    @Override
+    public ResourceDetail findById(Integer Id) {
+        return repository.getById(Id);
+    }
+
+    @Override
+    public List<ResourceDetail> findAll() {
+        return repository.findAll();
+    }
+
+    //TODO
+    @Override
+    public List<ResourceDetail> findByResource(Integer resourceId) {
+        // return repository.findByResourceId(resourceId);
+        return null;
+    }
+    //TODO
+    @Override
+    public List<ResourceDetail> findByProjectColumn(Integer projectColumnId) {
+        // return repository.findByProjectColumnId(projectColumnId);
+        return null;
+    }
+
+    @Override
+    public ResourceDetail save(ResourceDetail resourceDetail) {
+        return repository.save(resourceDetail);
+    }
 }
